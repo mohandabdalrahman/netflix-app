e6fd93132177a1e98cbe427f6b3c58284f319b5c
mohand <mohand.abdalrahman94@yahoo.com>
2020-10-06 22:27:54 +0200

    init firebase

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 import { GlobalStyle } from './global-styles'
+import { firebase } from './lib/firebase'
+import { FirebaseContext } from './context/firebase'
 import 'normalize.css'
 ReactDOM.render(
   <BrowserRouter>
-    <GlobalStyle />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyle />
+      <App />
+    </FirebaseContext.Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
